8a232f1de2471bbf3c894ec8eb0435322debe129
kubeway <kubeway@gmail.com>
2019-06-16 11:52:27 +0800

    fix(fix): fix bug
     1. add image repo
     2. fix configmap key error

--- create-github-trigger.sh
+++ create-github-trigger.sh
@@ -6,6 +6,7 @@
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 module_name=""
 code_repo=""
+image_repo=""
 save_path=""
 
 function print_help()
@@ -19,12 +20,14 @@ function print_help()
             example helloworld-go
         -s: github-trigger save path 
             example /tmp/github-trigger/
+        -i: image repo
+            example registry.cn-hangzhou.aliyuncs.com/knative-sample/helloworld-go
     Usage: ${BASH_SOURCE[0]} -c knative-sample/helloworld-go -m helloworld-go -s /tmp/github-trigger/
     Author: kubeway
 EOF
 }
 
-while getopts "c:m:s:" opt; do
+while getopts "c:m:s:i:" opt; do
     case $opt in
         m)
             module_name=${OPTARG}
@@ -34,6 +37,9 @@ while getopts "c:m:s:" opt; do
             ;;
         s)
             save_path=${OPTARG}
+            ;;
+        i)
+            image_repo=${OPTARG}
             ;;
          *)
              echo "unknown: "$opt""
@@ -45,6 +51,12 @@ while getopts "c:m:s:" opt; do
 
 if [ -z "${code_repo}" ]; then
     echo "code_repo is empty"
+    print_help
+    exit 1
+fi
+
+if [ -z "${image_repo}" ]; then
+    echo "image_repo is empty"
     print_help
     exit 1
 fi
@@ -71,13 +83,9 @@ while read origin_file; do
         -v module_name="${module_name}" \
         -v save_path="${save_path}" \
         -v file_name="${file_name}" \
-        '{print "sed \"s/{{.ModuleName}}/"module_name"/g\" "$1"| sed \"s?{{.CodeRepo}}?"code_repo"?g\" >"save_path""file_name}'|bash
+        -v image_repo=${image_repo} \
+        '{print "sed \"s/{{.ModuleName}}/"module_name"/g\" "$1"| sed \"s?{{.ImageRepo}}?"image_repo"?g\" | sed \"s?{{.CodeRepo}}?"code_repo"?g\" >"save_path""file_name}'|bash
     echo "create ${save_path}${file_name} success"
 done << EOF
 $(ls ${ROOTDIR}/*.yaml )
 EOF
-# ls ${ROOTDIR}/*.yaml |  \
-#     awk -v code_repo="${code_repo}" \
-#         -v module_name="${module_name}" \
-#         -v save_path="${save_path}" \
-#         '{print "sed \"s/{{.ModuleName}}/"module_name"/g\" "$1 "| sed \"s?{{.CodeRepo}}?"code_repo"?g\" >"save_path""$1}'
